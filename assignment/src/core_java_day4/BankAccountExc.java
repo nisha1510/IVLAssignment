52f63b5546c38ad5ba0fb3dd51e7a6d950b174e0
nisha1510 <jadhavnisha15@gmail.com>
2025-06-12 23:14:36 +0530

    exc

--- assignment/src/core_java_day4/BankAccountExc.java
+++ assignment/src/core_java_day4/BankAccountExc.java
@@ -60,15 +60,47 @@ public class BankAccountExc {
 		}
 	}
 	public static void main(String[] args) {
-		BankAccountExc acc = new BankAccountExc(101,"Nisha",1000);
-		acc.deposit(2000);
-		System.out.println("--------------");
-		acc.withdraw(4000);
-		System.out.println("--------------");
-		acc.deposit(2000);
-		System.out.println("--------------");
-		acc.withdraw(4000);
+//		BankAccountExc acc = new BankAccountExc(101,"Nisha",1000);
+//		acc.deposit(2000);
+//		System.out.println("--------------");
+//		acc.withdraw(4000);
+//		System.out.println("--------------");
+//		acc.deposit(2000);
+//		System.out.println("--------------");
+//		acc.withdraw(4000);
 		
+		BankAccountExc acc = null;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account Id :");
+		int id = sc.nextInt();
+		
+		System.out.println("Enter Account Holder's name:");
+        String name = sc.next();
+        
+//		System.out.println("Enter balance : ");
+//		double balance=sc.nextDouble();
+		
+		while (true) {
+            System.out.println("\nChoose Transaction: 1 for Deposit, 2 for Withdrawal, 3 to Exit");
+            int transactionChoice = sc.nextInt();
 
+            if (transactionChoice == 3) {
+                System.out.println("Thank you for using our banking system! ");
+                break;
+            }
+
+            System.out.println("Enter Amount:");
+            double amount = sc.nextDouble();
+            
+            if (transactionChoice == 1) {
+                acc.deposit(amount);
+            } else if (transactionChoice == 2) {
+                acc.withdraw(amount);
+            } else {
+                System.out.println(" Invalid transaction choice! Please select again.");
+            }
+		}
 	}
 }
