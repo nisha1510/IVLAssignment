5d876c6c6bb05846563f85010624b389e0ab61d1
nisha1510 <jadhavnisha15@gmail.com>
2025-06-12 12:16:35 +0530

    inprogress

---
+++ assignment/src/assignment02BankAppExtended/BankAccount.java
@@ -0,0 +1,10 @@
+package assignment02BankAppExtended;
+
+public abstract class BankAccount {
+	
+	String accHolderName;
+	String accNumber;
+	String mobileNumber;
+	double balance;
+	boolean isACtive;
+}
