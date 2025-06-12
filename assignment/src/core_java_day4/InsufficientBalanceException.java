9e039f23c3bb5233f84f9d928415a99bf3dbcc9b
nisha1510 <78904398+nisha1510@users.noreply.github.com>
2025-06-12 14:09:52 +0530

    Update InsufficientBalanceException.java

--- assignment/src/core_java_day4/InsufficientBalanceException.java
+++ assignment/src/core_java_day4/InsufficientBalanceException.java
@@ -1,10 +1,11 @@
 package core_java_day4;
 
 public class InsufficientBalanceException {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public InsufficientBalanceException() {	
+	}
+	public InsufficientBalanceException(String msg) {
+		System.out.println(msg);
 	}
 
 }
